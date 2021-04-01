547b5737d170e21857f9425d63fd5850045cd524
Xanonymous <trusaidlin@gmail.com>
2021-04-01 23:18:34 +0800

    hw2 p1

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,4 +1,5 @@
 import sys
+import timeit as ti
 
 sys.setrecursionlimit(10 ** 6)
 
@@ -31,6 +32,67 @@ def iterative_multiplication(m: int, n: int) -> int:
     return result
 
 
+class AutoIncreasingInteger(int):
+    def __init__(self, value: int = 0):
+        super().__init__()
+        self.value = value
+
+    def __repr__(self) -> int:
+        current = self.value
+        self.value += 1
+        return current
+
+    def __str__(self):
+        return str(self.__repr__())
+
+
+def rec_and_iter_test():
+    executions = [recursive_multiplication, iterative_multiplication]
+    execution_index: AutoIncreasingInteger = AutoIncreasingInteger(0)
+    execution_data: [tuple[tuple[int, int]]] = (
+        ((69, 0), (491, 86), (27, 134), (85, 69), (4, 99)),
+        ((0, 9487), (99, 47), (423, 19), (5, 2), (311, 13)),
+        ((251, 79), (957, 60), (55, 8), (60, 54), (811, 40)),
+        ((954, 74), (54, 23), (478, 45), (12, 2), (31, 3)),
+        ((9, 987), (29, 41), (323, 59), (4, 88), (111, 11))
+    )
+
+    def function(test_number: int):
+        [executions[execution_index](*execution_data[test_number][k % 5]) for k in range(100)]
+
+    # ---------------------------------------------------------Method 1
+    print()
+    print("Running time comparison:")
+    print()
+
+    t_rec_total = 0
+    t_ite_total = 0
+
+    for c in range(5):
+
+        t_start = ti.default_timer()
+        for i in range(10000):
+            function(c)
+        tend = ti.default_timer()
+        t_rec = tend - t_start
+
+        t_start = ti.default_timer()
+        for i in range(10000):
+            function(c)
+        tend = ti.default_timer()
+        t_ite = tend - t_start
+
+        print()
+        print("Recursive approach test {0}:".format(c + 1), t_rec)
+        t_rec_total += t_rec
+        print("iterative approach test {0}:".format(c + 1), t_ite)
+        t_ite_total += t_ite
+        print()
+    print('total rec time:', t_rec_total)
+    print('total ite time:', t_ite_total)
+    print('total time:', t_ite_total + t_rec_total)
+
+
 def run():
     print(recursive_multiplication(2, 5) == 10)
     print(recursive_multiplication(3114, 3114) == 9696996)
@@ -45,3 +107,9 @@ def run():
     print(iterative_multiplication(0, 5) == 0)
     print(iterative_multiplication(2, 5) == 10)
     print(iterative_multiplication(8, 9) != 10)
+
+    a = AutoIncreasingInteger()
+    print(a)
+    print(a)
+
+    rec_and_iter_test()
